30d7b81c047f844ad6641c12fe07ff9fae7185e4
unknown <Administrator@N203_P20_Tiny.M2Schule.local>
2026-04-30 15:12:35 +0200

    fix 218

--- path_3d.gd
+++ path_3d.gd
@@ -1,16 +1,17 @@
 extends Node3D
-
 @onready var path: Path3D = $"."
-
 func _ready() -> void:
 	if path == null:
 		return
-
+	var schiene = load("res://schiene.obj")
 	var length := path.curve.get_baked_length()
+	var step := 0.5
 	var d := 0.0
-
+	var prev_pos: Vector3
+	var first := true
 	while d <= length:
 		var pos := path.curve.sample_baked(d)
+		var rot := path.curve.sample_baked_with_rotation()
 		if not first:
 			var mid := (prev_pos + pos) / 2.0
 			var diff := pos - prev_pos
@@ -19,7 +20,8 @@ func _ready() -> void:
 			rail.mesh = schiene
 			rail.position = mid
 			rail.scale = Vector3(dist, 1.0, 1.0)
-			rail.rotation_degrees = Vector3i(pos)
+			rail.rotation_degrees = Vector3i(rotation)
+			print(rail.rotation_degrees)
 			rail.basis = Basis.looking_at(diff.normalized(), Vector3.UP)
 			add_child(rail)
 		prev_pos = pos
